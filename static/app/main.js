8479941149dbf96b9e9123303736738993763650
Clarisse <clarisseisnard@gmail.com>
2024-09-06 17:11:59 -0300

    changes to js for the mobile menu and other css alterations for mobile responsiveness

--- static/app/main.js
+++ static/app/main.js
@@ -1,67 +1,90 @@
+// Mobile menu configuration ----------------------------------------------------------------------------------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', function() {
-    const nav = document.querySelector(".navbar"),
-      navOpenBtn = document.querySelector("#bar-open"),
-      navCloseBtn = document.querySelector("#x-closed");
-  
-    // Ensure elements are not null before adding event listeners
-    if (navOpenBtn) {
-      navOpenBtn.addEventListener("click", () => {
-        nav.classList.add("openNav");
-        console.log("clicked");
-      });
-    }
-  
-    if (navCloseBtn) {
-      navCloseBtn.addEventListener("click", () => {
-        nav.classList.remove("openNav");
-      });
-      console.log("clicked");
-    }
-  });
+  const nav = document.querySelector(".navbar"),
+    navOpenBtn = document.querySelector("#bar-open"),
+    navCloseBtn = document.querySelector("#x-closed");
 
-window.onload = function () {
-    var imgs = [];
-    imgs[0] = 'static/app/backgroundimgs/BG1.jpg';
-    imgs[1] = 'static/app/backgroundimgs/BG2.jpg';
-    imgs[2] = 'static/app/backgroundimgs/BG3.jpg';
-    imgs[3] = 'static/app/backgroundimgs/bg4.jpg';
-    imgs[4] = 'static/app/backgroundimgs/bg5.jpg';
-    imgs[5] = 'static/app/backgroundimgs/bg6.jpg';
-    imgs[6] = 'static/app/backgroundimgs/bg7.jpg';
-    imgs[7] = 'static/app/backgroundimgs/bg8.jpg';
-    imgs[8] = 'static/app/backgroundimgs/bg9.jpg';
-    imgs[9] = 'static/app/backgroundimgs/bg10.jpg';
-    imgs[10] = 'static/app/backgroundimgs/bg11.jpg';
-    
-    var x = Math.floor(Math.random() * imgs.length);
-    var background = document.getElementsByClassName("background-container")[0];
-    background.style.backgroundImage = `url(${imgs[x]})`;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  const overlay = document.querySelector("#overlay"),
-        popupscreen = document.querySelector(".pop-up-download"),
-        xbutton = document.querySelector("#closepopup"),
-        popup = document.querySelector(".popup");
-
-  // Ensure both elements are found
-  if (popupscreen && popup) {
-    // Add event listener to the popup element
-    popup.addEventListener("click", function() {
-      popupscreen.classList.add("openpopup");
-      overlay.classList.add("openpopup");
-      // $('body').toggleClass('hiddenOverflow');
+  // Ensure elements are not null before adding event listeners
+  if (navOpenBtn) {
+    navOpenBtn.addEventListener("click", () => {
+      nav.classList.add("openNav");
+      $("body").css("position", "fixed");
+      $('body').addClass("fixed-position");
+      console.log("mobile menu opened");
       $("body").css("overflow", "hidden");
-      console.log("Popup opened");
-    });
-  } 
-
-  if (xbutton) {
-    xbutton.addEventListener("click", function() {
-      popupscreen.classList.remove("openpopup");
-      overlay.classList.remove("openpopup");
-      $("body").css("overflow", "");
-      console.log('closed');
+      console.log("overflowhidden")
     });
   }
+
+  if (navCloseBtn) {
+    navCloseBtn.addEventListener("click", () => {
+      nav.classList.remove("openNav");
+      $("body").css("position", "");
+      $('body').removeClass("fixed-position");
+      console.log("mobile menu closed");
+      $("body").css("overflow", "");
+      console.log("overflow working again")
+    });
+  }
+
+  $(".responsive-nav-items > li > a").click(function() {
+    // $(".responsive-nav-items").css("display", "none");
+    nav.classList.remove("openNav")
+    $("body").css("position", "");
+    $('body').removeClass("fixed-position");
+    $("body").css("overflow", "");
+    console.log('mobile menu auto close')
+  })
+
+});
+
+// Changing background image on reload -------------------------------------------------------------------------------------------------------------------------------------------
+
+window.onload = function () {
+  var imgs = [];
+  imgs[0] = 'static/app/backgroundimgs/BG1.jpg';
+  imgs[1] = 'static/app/backgroundimgs/BG2.jpg';
+  imgs[2] = 'static/app/backgroundimgs/BG3.jpg';
+  imgs[3] = 'static/app/backgroundimgs/bg4.jpg';
+  imgs[4] = 'static/app/backgroundimgs/bg5.jpg';
+  imgs[5] = 'static/app/backgroundimgs/bg6.jpg';
+  imgs[6] = 'static/app/backgroundimgs/bg7.jpg';
+  imgs[7] = 'static/app/backgroundimgs/bg8.jpg';
+  imgs[8] = 'static/app/backgroundimgs/bg9.jpg';
+  imgs[9] = 'static/app/backgroundimgs/bg10.jpg';
+  imgs[10] = 'static/app/backgroundimgs/bg11.jpg';
+  
+  var x = Math.floor(Math.random() * imgs.length);
+  var background = document.getElementsByClassName("background-container")[0];
+  background.style.backgroundImage = `url(${imgs[x]})`;
+}
+
+// Popup on catalogos page ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+const overlay = document.querySelector("#overlay"),
+      popupscreen = document.querySelector(".pop-up-download"),
+      xbutton = document.querySelector("#closepopup"),
+      popup = document.querySelector(".popup");
+
+// Ensure both elements are found
+if (popupscreen && popup) {
+  // Add event listener to the popup element
+  popup.addEventListener("click", function() {
+    popupscreen.classList.add("openpopup");
+    overlay.classList.add("openpopup");
+    // $('body').toggleClass('hiddenOverflow');
+    $("body").css("overflow", "hidden");
+    console.log("Popup opened");
+  });
+} 
+
+if (xbutton) {
+  xbutton.addEventListener("click", function() {
+    popupscreen.classList.remove("openpopup");
+    overlay.classList.remove("openpopup");
+    $("body").css("overflow", "");
+    console.log('Popup closed');
+  });
+}
 });
